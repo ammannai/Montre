c9d6ae4327200eb0eef27e1e67fc5a3eebef1143
ammannai <aminemannai.essect@gmail.com>
2020-04-12 22:44:54 +0100

    adding reactive forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,32 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { User } from '../user';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { User } from "../user";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.css"],
 })
 export class SignupComponent implements OnInit {
-user : User ;
-id : number ;
-formBuilder : FormBuilder;
-userForm : FormGroup ;
-  constructor() { }
+  user: User;
+  id: number;
+  userForm: FormGroup;
+  constructor(
+     private formBuilder: FormBuilder  ) {}
 
   ngOnInit() {
-    this.user = new User("","","","","","");
+    this.user = new User("", "", "", "", "", "");
     this.userForm = this.formBuilder.group({
-      id : [""],
-firstName:[""],
-lastName:[""],
-email:[""],
-tel : [""],
-password : [""],
-confirmPassword : [""]
-    }
-    )}
-saveUser (User : any){
-  console.log("User added succefully",this.user);
-}
+     id: [""],
+      firstName: [
+        "",
+        [Validators.required, Validators.maxLength(8), Validators.minLength(2)],
+      ],
+       lastName: ["",Validators.required],
+       email: ["",Validators.required,Validators.email],
+       tel: ["",Validators.required,Validators.minLength(8)],
+       password: ["",Validators.required,Validators.minLength(5)],
+       confirmPassword: ["",Validators.required,Validators.minLength(5)],
+    });
+  }
+  saveUser(User: any) {
+    console.log("User added succefully", this.user);
+  }
 }
